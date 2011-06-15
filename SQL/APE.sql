d6d468a6c45e0fd40adf8bc5012f40a7eb50ac92
cgmendozag@gmail.com <cgmendozag@gmail.com@85c7205a-8606-27a7-450f-c202d1a749ea>
2011-06-15 23:20:14 +0000

    view

--- SQL/APE.sql
+++ SQL/APE.sql
@@ -379,6 +379,45 @@ LOCK TABLES `destinatario` WRITE;
 /*!40000 ALTER TABLE `destinatario` DISABLE KEYS */;
 /*!40000 ALTER TABLE `destinatario` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `producto_precio_stock`
+--
+
+DROP TABLE IF EXISTS `producto_precio_stock`;
+/*!50001 DROP VIEW IF EXISTS `producto_precio_stock`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `producto_precio_stock` (
+  `id_producto` int(11),
+  `nombre` varchar(45),
+  `valor` float unsigned zerofill,
+  `cantidad` int(10) unsigned zerofill
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Final view structure for view `producto_precio_stock`
+--
+
+/*!50001 DROP TABLE IF EXISTS `producto_precio_stock`*/;
+/*!50001 DROP VIEW IF EXISTS `producto_precio_stock`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `producto_precio_stock` AS select `a`.`id_producto` AS `id_producto`,`a`.`nombre` AS `nombre`,`b`.`valor` AS `valor`,`c`.`cantidad` AS `cantidad` from ((`producto` `a` left join `precio_producto` `b` on((`a`.`id_producto` = `b`.`id_producto`))) left join `stock` `c` on((`a`.`id_producto` = `c`.`id_producto`))) where (`b`.`fecha_creacion` = (select max(`precio_producto`.`fecha_creacion`) from `precio_producto` where (`precio_producto`.`id_producto` = `a`.`id_producto`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Dumping routines for database 'ape'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -389,4 +428,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-06-16  1:14:34
+-- Dump completed on 2011-06-16  1:19:59
